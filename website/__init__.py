997b7a5e1d739fce7af161c46472a685f022b1d0
andrewj-brown <92134285+andrewj-brown@users.noreply.github.com>
2023-08-16 19:15:23 +1000

    let's see if it actually deploys

--- website/__init__.py
+++ website/__init__.py
@@ -6,8 +6,10 @@ from flask import Flask
 
 
 def main(args):
-    logging.basicConfig(level=logging.DEBUG);
+    logging.basicConfig(level=logging.INFO);
     
     app = Flask(__name__, static_folder=None);
     app.config["SERVER_NAME"] = os.environ.get("APP_BASE_URL")
     waitress.serve(app, host=args[1], port=443)
+
+    logging.info("Website is up and awaiting requests.")
