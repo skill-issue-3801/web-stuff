47f08d1e5c530bc57c6efd4b1fef11916969fd22
andrewj-brown <92134285+andrewj-brown@users.noreply.github.com>
2023-08-18 18:35:25 +1000

    switch back to 9003

--- website/__init__.py
+++ website/__init__.py
@@ -10,6 +10,6 @@ def main(args):
     
     app = Flask(__name__, static_folder=None);
     app.config["SERVER_NAME"] = os.environ.get("APP_BASE_URL")
-    waitress.serve(app, host=args[1], port=443)
+    waitress.serve(app, host=args[1], port=9003)
 
     logging.info("Website is up and awaiting requests.")
