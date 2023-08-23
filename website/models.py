2ce2e79e00ada2a0059673fb830fbcc6225dd60f
andrewj-brown <92134285+andrewj-brown@users.noreply.github.com>
2023-08-23 14:21:22 +1000

    start to add db models

---
+++ website/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String
+
+class Base(DeclarativeBase):
+    pass
+
+class CalendarLocations(Base):
+    family_member_name: Mapped[str] = mapped_column("name", String, primary_key=True)
+    url: Mapped[str] = mapped_column("url", String, primary_key=True)
