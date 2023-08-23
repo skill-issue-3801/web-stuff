451a5a815f0cd964791f3dca1a066f1cbc8121de
andrewj-brown <92134285+andrewj-brown@users.noreply.github.com>
2023-08-23 17:42:05 +1000

    skeleton website started

---
+++ website/admin.py
@@ -0,0 +1,21 @@
+import logging
+
+from flask import Blueprint
+
+from .models import CalendarLocations
+from .app import app
+
+
+admin = Blueprint("admin", __name__)
+logger = logging.getLogger(__name__)
+
+
+@admin.route('/add_calendars', methods=["GET", "POST"], subdomain="<subdomain>")
+def add_calendars():
+    if request.method == "GET":
+        return "<p>This is the page to add calendars.</p>"
+    
+    db_session = app.db_session()
+    db_session.add(CalendarLocations(name="NAME", url="URL"))
+    db_session.commit()
+    db_session.close()
--- website/app.py
+++ website/app.py
@@ -2,19 +2,19 @@ from flask import Flask
 from sqlalchemy.orm import sessionmaker
 
 from .models import CalendarLocations
+from .admin import admin
 
 
 class DecoWebsite(Flask):
     def set_db_engine(self, db_engine):
         self.db_session = sessionmaker(bind=db_engine)
+    def send_static_file(self, filename: str, subdomain=None):
+        return super(DecoWebsite, self).send_static_file(filename)
 
 
 app = DecoWebsite(__name__, static_folder=None)
-
+app.register_blueprint(admin, url_prefix="/admin")
 
 @app.route("/")
 def hello_world():
-    db_session = app.db_session()
-    query = db_session.query(CalendarLocations)
-
-    return f"<p>Hello, World!</p>\n{query}"
+    return "<p>Hello, World!</p>"
