e3f838312ee02129789cc58a09c968e7dd5bc37f
Elias Blanch <e.blanch@uqconnect.edu.au>
2023-08-26 17:26:33 +1000

    Playing with templates and font
    - Added a main css file and added the associated fonts/colours as varibles
    - Made app.py run calendar attempt 1
    - Need to work out issues with static css

--- website/app.py
+++ website/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy.orm import sessionmaker
 
 from .models import CalendarLocations
@@ -16,8 +16,11 @@ app = DecoWebsite(__name__, static_folder=None)
 
 
 @app.route("/")
-def hello_world():
-    return """<p>
-The website for deco3801 2023 team 'skill issue'.
-Check out <a href="/admin">the admin page</a>.
-    </p>"""
+def home():
+    return render_template('calendar-attempt-1.html')
+# web-stuff\website\Front-end\calendar-attempt-1.html
+
+# def hello_world():
+#     return """ <link rel="stylesheet" href="url_for('static', filename='/Front-end/css/main.css'">
+#     <p> The website for deco3801 2023 team 'skill issue'. Check out <a href="/admin">the admin page</a>.
+#     </p>"""
