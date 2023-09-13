e3322cc4c0336bd34b72de18ca94200824d07150
kiahjardine <kiah.jardine@gmail.com>
2023-09-13 17:06:55 +1000

    moved do update to a different route so now we can just get the events json file

--- website/calendar.py
+++ website/calendar.py
@@ -1,5 +1,6 @@
 import logging
 import pytz
+import json
 from .cal import *
 from datetime import datetime
 from .base import has_global_stuff, has_db
@@ -22,24 +23,13 @@ def default(db_session, globals):
     anyChanges = False
     today = datetime.utcnow().replace(tzinfo=pytz.utc).date()
     hashes = {}
-    for member in family:
-        newHash = check_cal_for_updates(member.url, member.calendarType, member.eventsHash, today)
-        if newHash == False:
-            hashes[member.name] = member.eventsHash
-        else:
-            hashes[member.name] = newHash
-            anyChanges = True
+    check_for_update(family, anyChanges, today, hashes)
+    
     if anyChanges or globals.familyChanges:
-        #unpickle
-        userObjects = {}
-        for member in family:
-            userObjects[member.name] = member.userObject
-        evs = do_update(userObjects.values(), today, hashes)   
-        for member in family:
-            member.eventsHash = hashes[member.name]
-            member.userObject = userObjects[member.name]
-        globals.set_events(calendarise_events(evs))
-    db_session.commit()
+        jsonfile = do_update(family, today, hashes)
+        globals.set_events(json.loads(jsonfile))
+        db_session.commit()
+    
     globals.familyChanges = False
     anyChanges = False
     log_events(globals.events)
@@ -48,26 +38,49 @@ def default(db_session, globals):
         familyMembers[person.name] = person
     return render_template("calendar.html", events = globals.events, family = familyMembers)
 
-def do_update(userObjects, today, hashes):
-    logging.warning("doing update")
+def check_for_update(family, anyChanges, today, hashes):
+    for member in family:
+        newHash = check_cal_for_updates(member.url, member.calendarType, member.eventsHash, today)
+        if newHash == False:
+            hashes[member.name] = member.eventsHash
+        else:
+            hashes[member.name] = newHash
+            anyChanges = True
+
+def do_update(family, today, hashes):
+    userObjects = {}
+    for member in family:
+        userObjects[member.name] = member.userObject  
     names = []
     emails = []
-    for user in userObjects:
+    for user in userObjects.values():
         names.append(user.get_name())
         if user.get_email() != None:
             emails.append(user.get_email())
-    return update_events(userObjects, today, hashes, names, emails)
+    evs = update_events(userObjects.values(), today, hashes, names, emails)
+    for member in family:
+        member.eventsHash = hashes[member.name]
+        member.userObject = userObjects[member.name]
+    return(json.dumps(calendarise_events(evs)))
 
 def log_events(events):
     logging.warning("Events:")
     for event in events:
-        logging.warning("{} {} {} {}".format(event.summary, event.start, event.uid, str(event.attendees)))
+        logging.warning("{} {} {} {}".format(event['summary'], event['start'], event['uid'], str(event['attendees'])))
 
     
 
 @calendar.route("/do_update")
 @has_global_stuff
-@has_db
-def update(session, global_state):
-    global_state.set_events(global_state.events + ["xyz"])
-    return str(global_state.events)
+def update(db_session, globals):
+    family = db_session.query(FamilyMember).all()
+    anyChanges = False
+    today = datetime.utcnow().replace(tzinfo=pytz.utc).date()
+    hashes = {}
+    check_for_update(family, anyChanges, today, hashes)
+    
+    if anyChanges or globals.familyChanges:
+        jsonfile = do_update(family, today, hashes)
+        return(jsonfile)
+    else:
+        return(json.dumps(globals.events))
