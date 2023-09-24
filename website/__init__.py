1daa7f55ac0714b7267291e8794f22b9428bb5f5
kiahjardine <141206736+kiahjardine@users.noreply.github.com>
2023-09-24 15:18:07 +1000

    added extra test calendars

--- website/__init__.py
+++ website/__init__.py
@@ -57,6 +57,39 @@ def main(db_url, args):
                 eventsHash=eventsHash,
                 userObject=userObject,
             ))
+            name="testa"
+            url="https://calendar.google.com/calendar/ical/8b3d1384c9c8c2b83d89bbdfc618e9643c020a37f917797154bb8bc15313d399%40group.calendar.google.com/private-61d6eaf479c9245fe2746f578b6d9b3b/basic.ics"
+            calendarType="google"
+            email=None
+            icon="graphics/ocean-icons/Slug_1.png"
+            eventsHash = 0
+            userObject = User(name, url, calendarType, email)
+            s.add(FamilyMember(name=name, url=url, calendarType=calendarType, email=email, icon=icon, eventsHash=eventsHash, userObject=userObject))
+            
+            name="testb"
+            url="https://calendar.google.com/calendar/ical/9062b4ce5a3b1b15ba9e901454417dc0362a7576c967b63d7aa2e0e12561cf04%40group.calendar.google.com/private-b1725a4a26321058c715e75c353d80ae/basic.ics"
+            icon="graphics/ocean-icons/Slug_2.png"
+            userObject = User(name, url, calendarType, email)
+            s.add(FamilyMember(name=name, url=url, calendarType=calendarType, email=email, icon=icon, eventsHash=eventsHash, userObject=userObject))
+            
+            name="testc"
+            url="https://calendar.google.com/calendar/ical/8330378fae6a0e018310a99cde5b9a96bd5ae359059078593bfc4259181d1222%40group.calendar.google.com/private-618208b88c6b36f5a5f75460c71f4aaa/basic.ics"
+            icon="graphics/ocean-icons/Slug_3.png"
+            userObject = User(name, url, calendarType, email)
+            s.add(FamilyMember(name=name, url=url, calendarType=calendarType, email=email, icon=icon, eventsHash=eventsHash, userObject=userObject))
+            
+            name="testd"
+            url="https://calendar.google.com/calendar/ical/9a10fca10921df108cb568fe942ef0b1d75095228d164104ea3880d830e6defc%40group.calendar.google.com/private-1ad1b63e3255bc1a06b816d93ece94b6/basic.ics"
+            icon="graphics/ocean-icons/Slug_4.png"
+            userObject = User(name, url, calendarType, email)
+            s.add(FamilyMember(name=name, url=url, calendarType=calendarType, email=email, icon=icon, eventsHash=eventsHash, userObject=userObject))
+            
+            name="teste"
+            url=None
+            icon="graphics/ocean-icons/Slug_5.png"
+            userObject = User(name, url, 'none', email)
+            s.add(FamilyMember(name=name, url=url, calendarType=calendarType, email=email, icon=icon, eventsHash=eventsHash, userObject=userObject))
+            
 
             s.commit()
             s.close()        
