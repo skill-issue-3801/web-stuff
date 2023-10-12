b8c776f9459ad8f6a0a1a04299b8c7781ac20a9d
kiahjardine <141206736+kiahjardine@users.noreply.github.com>
2023-10-12 21:34:23 +1000

    new and improved users

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ from .calendar import calendar
 from .base import Session, DB
 from .models import Base, FamilyMember
 from .cal import User
-
+ 
 
 def main(db_url, args):
     logging.basicConfig(level=logging.INFO)
@@ -31,7 +31,7 @@ def main(db_url, args):
         with app.app_context():
             s = Session()
 
-            name = "Aiden"
+            name = "Rose"
             url = "https://calendar.google.com/calendar/ical/8b3d1384c9c8c2b83d89bbdfc618e9643c020a37f917797154bb8bc15313d399%40group.calendar.google.com/private-61d6eaf479c9245fe2746f578b6d9b3b/basic.ics"
             calendarType = "google"
             email = None
@@ -52,7 +52,7 @@ def main(db_url, args):
 
             name = "Eden"
             url = "https://calendar.google.com/calendar/ical/9062b4ce5a3b1b15ba9e901454417dc0362a7576c967b63d7aa2e0e12561cf04%40group.calendar.google.com/private-b1725a4a26321058c715e75c353d80ae/basic.ics"
-            icon = "graphics/ocean-icons/Slug_3.png"
+            icon = "graphics/ocean-icons/Slug_6.png"
             userObject = User(name, url, calendarType, email)
             s.add(
                 FamilyMember(
@@ -84,7 +84,7 @@ def main(db_url, args):
 
             name = "Wendall"
             url = "https://calendar.google.com/calendar/ical/9a10fca10921df108cb568fe942ef0b1d75095228d164104ea3880d830e6defc%40group.calendar.google.com/private-1ad1b63e3255bc1a06b816d93ece94b6/basic.ics"
-            icon = "graphics/ocean-icons/Slug_6.png"
+            icon = "graphics/ocean-icons/Slug_3.png"
             userObject = User(name, url, calendarType, email)
             s.add(
                 FamilyMember(
