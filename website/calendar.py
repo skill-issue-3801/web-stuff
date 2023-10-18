e6ce58a27da7b2fc9ed1dad2ad296c36ae5b1aa6
kiahjardine <141206736+kiahjardine@users.noreply.github.com>
2023-10-18 20:14:53 +1000

    Revert "capital letters???"
    This reverts commit 6dff6a62dc7bea44c90ec1593c1c8f25668eb777.

--- website/calendar.py
+++ website/calendar.py
@@ -37,10 +37,10 @@ def default(db_session, globals):
     anyChanges = False
     log_events(globals.events)
     familyMembers = {}
-    peoplesindex = {}
+    peoplesIndex = {}
     for person in family:
         familyMembers[person.name] = person
-        peoplesindex[person.name] = person.iconindex
+        peoplesIndex[person.name] = person.iconindex
     firstDay = today - timedelta(days=((today.weekday() + 1) % 7))
     dates = []
     for i in range(0, 7):
@@ -54,7 +54,7 @@ def default(db_session, globals):
         family=familyMembers,
         todayIndex=(today.weekday() + 1) % 7,
         firstDay = firstDay,
-        peoplesindex=peoplesindex,
+        peoplesIndex=peoplesIndex,
         uids=uids_to_div_dict(family),
         homeAndAway=build_away_array(today, globals.events[math.floor(total_weeks_loaded /2)])
     )
